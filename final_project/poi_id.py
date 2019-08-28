5bd6ebeb378ce2a95c61ec6fc4be1367aff52fa1
Rohit Kr Singh <ht97kumarrk@gmail.com>
2019-08-28 14:12:35 +0530

    Determine a Confusion Matrix, Recall value, etc on test set

--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -7,6 +7,14 @@ sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data
 
+
+PERF_FORMAT_STRING = "\
+\tAccuracy: {:>0.{display_precision}f}\tPrecision: {:>0.{display_precision}f}\t\
+Recall: {:>0.{display_precision}f}\tF1: {:>0.{display_precision}f}\tF2: {:>0.{display_precision}f}"
+RESULTS_FORMAT_STRING = "\tTotal predictions: {:4d}\tTrue positives: {:4d}\tFalse positives: {:4d}\
+\tFalse negatives: {:4d}\tTrue negatives: {:4d}"
+
+
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
@@ -53,7 +61,37 @@ features_train, features_test, labels_train, labels_test = \
     train_test_split(features, labels, test_size=0.3, random_state=42)
 clf.fit(features_train, labels_train)
 predictions = clf.predict(features_test)
-print(accuracy_score(labels_test, predictions))
+#print(accuracy_score(labels_test, predictions))
+
+true_negatives = 0
+false_negatives = 0
+true_positives = 0
+false_positives = 0
+for prediction, truth in zip(predictions, labels_test):
+	if prediction == 0 and truth == 0:
+		true_negatives += 1
+	elif prediction == 0 and truth == 1:
+		false_negatives += 1
+	elif prediction == 1 and truth == 0:
+		false_positives += 1
+	elif prediction == 1 and truth == 1:
+		true_positives += 1
+	else:
+		print("Warning: Found a predicted label not == 0 or 1.")
+		print("All predictions should take value 0 or 1.")
+		print("Evaluating performance for processed predictions:")
+		break
+
+total_predictions = true_negatives + false_negatives + false_positives + true_positives
+accuracy = 1.0*(true_positives + true_negatives)/total_predictions
+precision = 1.0*true_positives/(true_positives+false_positives)
+recall = 1.0*true_positives/(true_positives+false_negatives)
+f1 = 2.0 * true_positives/(2*true_positives + false_positives+false_negatives)
+f2 = (1+2.0*2.0) * precision*recall/(4*precision + recall)
+print()
+print(PERF_FORMAT_STRING.format(accuracy, precision, recall, f1, f2, display_precision = 5))
+print(RESULTS_FORMAT_STRING.format(total_predictions, true_positives, false_positives, false_negatives, true_negatives))
+print("")
 
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
